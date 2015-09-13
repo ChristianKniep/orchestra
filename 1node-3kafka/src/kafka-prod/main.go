98b3529c702593b7c1048f3869a6265dfd14a7c3
Christian Kniep <christian@qnib.org>
2015-09-13 22:33:55 +0200

    heka uses simple lua sandbox to check if the incremented number has incremented by one

--- 1node-3kafka/src/kafka-prod/main.go
+++ 1node-3kafka/src/kafka-prod/main.go
@@ -1,12 +1,20 @@
 package main
 
+// Send incrementing numbers to kafka topic
+// if an argument is given (anything) it randomly jumps ahead one number (R ~ 10%)
+
 import (
 	"fmt"
-    . "github.com/Shopify/sarama"
-    "time"
+	. "github.com/Shopify/sarama"
+	"math/rand"
+	"time"
+	"os"
 )
 
 func main() {
+	const sleep_ms = 1100
+	// random seed
+	random := rand.New(rand.NewSource(99))
 	producer, err := NewSyncProducer([]string{"kafka.service.consul:9092"}, nil)
 	if err != nil {
 		fmt.Println(err)
@@ -17,21 +25,31 @@ func main() {
 		}
 	}()
 
-    now := time.Now()                                   // 02 Apr 15 14:03
-    ts := fmt.Sprintf("%v", now.Format(time.RFC3339))
-    j := 0
-    var val int64
+	j := 0
+	jump := 0
+	args := os.Args[1:]
 	for {
-        txt := fmt.Sprintf("%v | Msg#%v > %v",ts, j, val)
-        msg := &ProducerMessage{Topic: "ring0", Value: StringEncoder(txt)}
-        partition, offset, err := producer.SendMessage(msg)
-        if err != nil {
-        	fmt.Printf("FAILED to send message: %s\n", err)
-        } else {
-        	fmt.Printf("> '%s' sent to partition %d at offset %d\n", txt, partition, offset)
-        }
-        time.Sleep(100 * time.Millisecond)
-        j = j + 1
-        val = offset + 1
-    }
+		if len(args) > 0 {
+			if random.Intn(10) == 1 {
+				j = j + 1
+				jump = 1
+			} else {
+				jump = 0
+			}
+		}
+		txt := fmt.Sprintf("%d", j)
+		msg := &ProducerMessage{Topic: "ring0", Value: StringEncoder(txt)}
+		partition, offset, err := producer.SendMessage(msg)
+		if err != nil {
+			fmt.Printf("FAILED to send message: %s\n", err)
+		} else {
+			if jump == 0 {
+				fmt.Printf("> '%s' sent to partition %d at offset %d\n", txt, partition, offset)
+			} else {
+				fmt.Printf("JUMPED AHEAD > '%s' sent to partition %d at offset %d\n", txt, partition, offset)
+			}
+		}
+		time.Sleep(sleep_ms * time.Millisecond)
+		j = j + 1
+	}
 }
