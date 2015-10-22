902e417c1fdae4757627f2db96526f3523e6dff2
Christian Kniep <christian@qnib.org>
2015-10-22 09:17:33 +0200

    full fletched nginx loadbalancer example

--- nginx-elk/www/usr/local/bin/update_html.sh
+++ nginx-elk/www/usr/local/bin/update_html.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+cd /usr/local/cats/
+PIC=$(find ./ -type f | shuf -n 1)
+cp ${PIC} /usr/share/nginx/html/
+
 echo "<h2>You are served by $(hostname)!</h2>" > /usr/share/nginx/html/index.html
+echo "<img src=\"${PIC}\"/>" >> /usr/share/nginx/html/index.html
