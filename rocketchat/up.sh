e6d5ac93b67a99f8fdc7a6d31e0e27199b146ea2
Christian Kniep <christian@qnib.org>
2015-11-29 12:51:53 +0100

    add rocketchat adjustments for MELIG3

---
+++ rocketchat/up.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+RKTHOST=$(echo $DOCKER_HOST |egrep -o "\d+\.\d+\.\d+\.\d+") docker-compose up -d consul mongodb
+sleep 5
+RKTHOST=$(echo $DOCKER_HOST |egrep -o "\d+\.\d+\.\d+\.\d+") docker-compose up -d rocketchat carbon gapi grafana
+sleep 30
+RKTHOST=$(echo $DOCKER_HOST |egrep -o "\d+\.\d+\.\d+\.\d+") docker-compose up -d
+
